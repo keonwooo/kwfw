35de47c0455e54717b9515f63061c8d55dc2ca46
keonwooo <rjsdn7732@naver.com>
2024-09-05 22:54:31 +0900

    [2024.09.05] - 김건우 1. 첫 프로젝트 commit

---
+++ board/src/test/java/com/kwfw/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwfw.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
